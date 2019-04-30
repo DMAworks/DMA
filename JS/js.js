e835808e1a52126d9f32973b9b6cddcda9b5045c
DMA_works <maga_dahkilgov@outlook.com>
2019-04-30 20:29:20 +0300

    JavaScript Project

--- JS/js.js
+++ JS/js.js
@@ -1,32 +1,119 @@
-// function showFirstMessage(text) {
-//     alert(text);
-    
-// }
+// // // // // // function showFirstMessage(text) {
+// // // // // //     alert(text);
 
-function calc(a,b) {
-    return (a + b);
+// // // // // // }
+
+// // // // // function calc(a,b) {
+// // // // //     return (a + b);
+// // // // // }
+
+// // // // // console.log (calc(3,4));
+
+// // // // // console.log (calc(4,4));
+
+// // // // // function retVar() {
+// // // // //     let num = 50;
+// // // // //     return num;
+// // // // // }
+
+// // // // // let anotherNum = retVar();
+// // // // // console.log(anotherNum);
+
+
+// // // // // let str = "test";
+// // // // // console.log(str.length);
+
+// // // // // console.log(str.toUpperCase());
+// // // // // console.log(str.toLowerCase());
+
+// // // // // let twelve = "12.2px";
+// // // // // // console.log(Math.round(twelve));
+// // // // // console.log(parseInt(twelve));
+// // // // // console.log(parseFloat(twelve));
+
+// // // // // function learnJS(lang,callback) {
+// // // // //     console.log("I'm learning" + lang);
+// // // // //     callback();
+// // // // // }
+
+// // // // // function done() {
+// // // // //     console.log("I learnd 3-rd lesson!");
+// // // // // }
+
+// // // // // learnJS("JavaScript", done);
+
+// // // // let options = {
+// // // //     width: 1024,
+// // // //     height: 1024,
+// // // //     name: "test"
+// // // // };
+
+// // // // console.log(options.name);
+
+// // // // options.bool = false;
+// // // // options.colors = {
+// // // //     border: "black",
+// // // //     bg: "red"
+// // // // };
+
+// // // // delete options.bool;
+
+// // // // console.log(options);
+
+// // // // for(let key in options) {
+// // // //     console.log("Свойство " + key + " имеет значение " + options[key]);
+// // // // }
+
+// // // // console.log(Object.keys(options).length);
+
+// // // let arr = ["first" ,2 ,3 ,"four" ,5];
+
+
+// // // // for (let i = 0; i < arr.length; i++) {
+// // // //     console.log(arr[i]);
+// // // // }
+
+// // // arr.forEach(function(item, i, mass) {
+// // //     console.log(i + " " + item + "(массив: " + mass + ")");
+// // // });
+
+// // // console.log(arr);
+
+// // // let mass = [1,2,3,4,5,6,7];
+
+// // // for (let key of mass) {
+// // //     console.log(key);
+// // // }
+
+// // let ans = prompt("", ""),
+// //     arr = [];
+
+// // arr = ans.split(",");   
+// // console.log(arr);
+
+// let arr = ["arr", "zzz", "asd", "rrr"],
+//     i = arr.join(", ");
+
+//     console.log(i);
+
+let arr = [1, 15, 4, 17],
+        i = arr.sort();
+
+function compareNum(a, b) {
+        return a - b;
 }
 
-console.log (calc(3,4));
+console.log(i);
 
-console.log (calc(4,4));
+let soldier = {
+        health: 400,
+        armor: 100
+};
 
-function retVar() {
-    let num = 50;
-    return num;
-}
+let john = {
+        health: 100
+};
 
-let anotherNum = retVar();
-console.log(anotherNum);
-
-
-let str = "test";
-console.log(str.length);
-
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-let twelve = "12.2px";
-// console.log(Math.round(twelve));
-console.log(parseInt(twelve));
-console.log(parseFloat(twelve));
+john.__proto__ = soldier;
+ console.log(john);
+ console.log(john.armor);       
--- JS/tempCodeRunnerFile.js
+++ JS/tempCodeRunnerFile.js
@@ -1,30 +1,95 @@
-// function showFirstMessage(text) {
-//     alert(text);
+// // // // // function showFirstMessage(text) {
+// // // // //     alert(text);
     
-// }
+// // // // // }
 
-function calc(a,b) {
-    return (a + b);
-}
+// // // // function calc(a,b) {
+// // // //     return (a + b);
+// // // // }
 
-console.log (calc(3,4));
+// // // // console.log (calc(3,4));
 
-console.log (calc(4,4));
+// // // // console.log (calc(4,4));
 
-function retVar() {
-    let num = 50;
-    return num;
-}
+// // // // function retVar() {
+// // // //     let num = 50;
+// // // //     return num;
+// // // // }
 
-let anotherNum = retVar();
-console.log(anotherNum);
+// // // // let anotherNum = retVar();
+// // // // console.log(anotherNum);
 
 
-let str = "test";
-console.log(str.length);
+// // // // let str = "test";
+// // // // console.log(str.length);
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
+// // // // console.log(str.toUpperCase());
+// // // // console.log(str.toLowerCase());
 
-let twelve = "12.2";
-console.log(Math.round(twelve));
+// // // // let twelve = "12.2px";
+// // // // // console.log(Math.round(twelve));
+// // // // console.log(parseInt(twelve));
+// // // // console.log(parseFloat(twelve));
+
+// // // // function learnJS(lang,callback) {
+// // // //     console.log("I'm learning" + lang);
+// // // //     callback();
+// // // // }
+
+// // // // function done() {
+// // // //     console.log("I learnd 3-rd lesson!");
+// // // // }
+
+// // // // learnJS("JavaScript", done);
+
+// // // let options = {
+// // //     width: 1024,
+// // //     height: 1024,
+// // //     name: "test"
+// // // };
+
+// // // console.log(options.name);
+
+// // // options.bool = false;
+// // // options.colors = {
+// // //     border: "black",
+// // //     bg: "red"
+// // // };
+
+// // // delete options.bool;
+
+// // // console.log(options);
+
+// // // for(let key in options) {
+// // //     console.log("Свойство " + key + " имеет значение " + options[key]);
+// // // }
+
+// // // console.log(Object.keys(options).length);
+
+// // let arr = ["first" ,2 ,3 ,"four" ,5];
+
+
+// // // for (let i = 0; i < arr.length; i++) {
+// // //     console.log(arr[i]);
+// // // }
+
+// // arr.forEach(function(item, i, mass) {
+// //     console.log(i + " " + item + "(массив: " + mass + ")");
+// // });
+
+// // console.log(arr);
+
+// // let mass = [1,2,3,4,5,6,7];
+
+// // for (let key of mass) {
+// //     console.log(key);
+// // }
+
+// let ans = prompt("", ""),
+//     arr = [];
+
+// arr = ans.split(",");   
+// console.log(arr);
+
+let arr = ["arr", "zzz", "asd", "rrr"],
+    i = arr.join(", ");
